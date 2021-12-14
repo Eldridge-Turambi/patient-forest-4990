4ab158880c0ed1f32dde494de22759d695b2434b
Eldridge Turambi <eturambi95@gmail.com>
2021-12-14 16:09:04 -0700

    Almost Finished User Story 3

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,21 @@
 class MoviesController < ApplicationController
 
+  def new
+
+  end
+
   def show
     @movie = Movie.find(params[:id])
   end
+
+  def create
+    @movie = Movie.find(params[:id])
+    Actor.create!(actor_params)
+    redirect_to "/movies/#{@movie.id}"
+  end
+
+  private
+    def actor_params
+      params.permit(:name, :age)
+    end
 end
--- spec/features/movies/show_spec.rb
+++ spec/features/movies/show_spec.rb
@@ -24,4 +24,22 @@ RSpec.describe 'Movie Show Page' do
 
     ## I do not know how to do the actors by age without using ordelry and instead using CSS. Can you help me??
   end
+
+  it 'sees a form to add an actor to the movie, fills out the form with actor that exists, click submit, then taken back to the show page and the actors name is listed' do
+    studio1 = Studio.create!(name: "Universal Studios", location: 'Hollywood')
+    movie1 = studio1.movies.create!(title: 'Pirates', creation_year: 2005, genre: 'Adventure')
+    johnny_depp = movie1.actors.create!(name: 'Johnny Depp', age: 58)
+    orlando_bloom = Actor.create!(name: 'Orlando Bloom', age: 44)
+    kiera_knightey = movie1.actors.create!(name: 'Kierra Knightley', age: 36 )
+    movieactor1 = MovieActor.create!(movie_id: movie1.id, actor: johnny_depp, actor: kiera_knightey)
+    visit "movies/#{movie1.id}"
+
+    fill_in "name", with: "Orlando Bloom"
+    fill_in "age", with: 44
+
+    click_button "Create Actor"
+
+    expect(current_path).to eq("/movies/#{movie1.id}")
+    expect(page).to have_content("Orlando Bloom")
+  end
 end
